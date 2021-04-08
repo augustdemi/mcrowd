f764024b7bdf17f3e60f3fad29fb8e7de0a31c5f
augustdemi <augustdemi@gmail.com>
2021-04-08 12:37:06 -0400

    4 conv+2fc, no velocity

--- model_map_ae.py
+++ model_map_ae.py
@@ -55,10 +55,23 @@ class Encoder(nn.Module):
     def __init__(self, fc_hidden_dim, output_dim, drop_out):
         super(Encoder, self).__init__()
         self.drop_out = drop_out
-        self.conv1 = nn.Conv2d(1, 4, 7, stride=3, bias=False)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(4, 4, 5, stride=2, bias=False)
-        self.fc1 = nn.Linear(4 * 7 * 7, fc_hidden_dim, bias=False)
+
+        # self.conv1 = nn.Conv2d(1, 4, 7, stride=3, bias=False)  # 198 ->64 -> 32
+        # self.conv2 = nn.Conv2d(4, 4, 5, stride=2, bias=False)
+        # self.fc1 = nn.Linear(4 * 7 * 7 + 2, fc_hidden_dim, bias=False)
+        # self.fc2 = nn.Linear(fc_hidden_dim, output_dim, bias=False)
+
+        # self.conv1 = nn.Conv2d(1, 4, 7, stride=1, bias=False) #192->96
+        # self.conv2 = nn.Conv2d(4, 4, 5, stride=1, bias=False) #92->46
+        # self.conv3 = nn.Conv2d(4, 4, 3, stride=1, bias=False) #44->22
+        # self.conv4 = nn.Conv2d(4, 4, 3, stride=1, bias=False) #20 ->10
+
+        self.conv1 = nn.Conv2d(1, 4, 7, stride=2, bias=False) #96 -> 48
+        self.conv2 = nn.Conv2d(4, 4, 5, stride=1, bias=False) #44 ->22
+        self.conv3 = nn.Conv2d(4, 4, 3, stride=1, bias=False) #20->10
+        self.conv4 = nn.Conv2d(4, 4, 3, stride=1, bias=False) # 8 -> 4
+        self.fc1 = nn.Linear(4 * 4 * 4, fc_hidden_dim, bias=False)
         self.fc2 = nn.Linear(fc_hidden_dim, output_dim, bias=False)
 
     def forward(self, state, map, train=False):
@@ -68,9 +81,11 @@ class Encoder(nn.Module):
         Output:
         - final_h: Tensor of shape (self.num_layers, batch, self.h_dim)
         """
-        x = self.pool(F.relu(self.conv1(map))) # 198 ->64 -> 32
-        x = self.pool(F.relu(self.conv2(x)))  # 14->7
-        x = x.view(-1, 4 * 7 * 7)
+        x = self.pool(F.relu(self.conv1(map)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = x.view(-1, 4 * 4 * 4)
         # x = torch.cat((x, state[:, 2:4]), -1)
         x = F.relu(self.fc1(x))
         obst_feat = self.fc2(x)
@@ -87,11 +102,21 @@ class Decoder(nn.Module):
     def __init__(self, fc_hidden_dim, input_dim):
         super(Decoder, self).__init__()
         self.fc1 = nn.Linear(input_dim, fc_hidden_dim, bias=False)
-        self.fc2 = nn.Linear(fc_hidden_dim, 4 * 7 * 7, bias=False)
-        self.upsample1 = nn.Upsample(14)
-        self.deconv1 = nn.ConvTranspose2d(4, 4, 6, stride=2, bias=False)
-        self.upsample2 = nn.Upsample(64)
-        self.deconv2 = nn.ConvTranspose2d(4, 1, 9, stride=3, bias=False)
+        self.fc2 = nn.Linear(fc_hidden_dim, 4 * 4 * 4, bias=False)
+        self.deconv = nn.Sequential(
+            nn.Upsample(8),
+            nn.ConvTranspose2d(4, 4, 3, stride=1, bias=False),
+            nn.ReLU(),
+            nn.Upsample(20),
+            nn.ConvTranspose2d(4, 4, 3, stride=1, bias=False),
+            nn.ReLU(),
+            nn.Upsample(44),
+            nn.ConvTranspose2d(4, 4, 5, stride=1, bias=False),
+            nn.ReLU(),
+            nn.Upsample(96),
+            nn.ConvTranspose2d(4, 1, 8, stride=2, bias=False),
+        )
+
 
     def forward(self, obst_feat):
         """
@@ -106,10 +131,7 @@ class Decoder(nn.Module):
         """
         x= self.fc1(obst_feat)
         x= self.fc2(F.relu(x))
-        x = x.view(-1, 4, 7, 7)
-        x = self.upsample1(F.relu(x))
-        x = self.deconv1(x) # 32, 32
-        x = self.upsample2(F.relu(x)) #64
-        map = self.deconv2(x) #198
+        x = x.view(-1, 4, 4, 4)
+        map = self.deconv(F.relu(x))
         return F.sigmoid(map)
 
