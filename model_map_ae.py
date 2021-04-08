e47ff3524fcb4f69d4dc6db904df702497ce490f
augustdemi <augustdemi@gmail.com>
2021-04-08 12:26:19 -0400

    original net - without velocity

--- model_map_ae.py
+++ model_map_ae.py
@@ -58,7 +58,7 @@ class Encoder(nn.Module):
         self.conv1 = nn.Conv2d(1, 4, 7, stride=3, bias=False)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(4, 4, 5, stride=2, bias=False)
-        self.fc1 = nn.Linear(4 * 7 * 7 + 2, fc_hidden_dim, bias=False)
+        self.fc1 = nn.Linear(4 * 7 * 7, fc_hidden_dim, bias=False)
         self.fc2 = nn.Linear(fc_hidden_dim, output_dim, bias=False)
 
     def forward(self, state, map, train=False):
@@ -71,7 +71,7 @@ class Encoder(nn.Module):
         x = self.pool(F.relu(self.conv1(map))) # 198 ->64 -> 32
         x = self.pool(F.relu(self.conv2(x)))  # 14->7
         x = x.view(-1, 4 * 7 * 7)
-        x = torch.cat((x, state[:, 2:4]), -1)
+        # x = torch.cat((x, state[:, 2:4]), -1)
         x = F.relu(self.fc1(x))
         obst_feat = self.fc2(x)
         obst_feat = F.dropout(obst_feat,
@@ -87,7 +87,7 @@ class Decoder(nn.Module):
     def __init__(self, fc_hidden_dim, input_dim):
         super(Decoder, self).__init__()
         self.fc1 = nn.Linear(input_dim, fc_hidden_dim, bias=False)
-        self.fc2 = nn.Linear(fc_hidden_dim, 4 * 7 * 7 + 2, bias=False)
+        self.fc2 = nn.Linear(fc_hidden_dim, 4 * 7 * 7, bias=False)
         self.upsample1 = nn.Upsample(14)
         self.deconv1 = nn.ConvTranspose2d(4, 4, 6, stride=2, bias=False)
         self.upsample2 = nn.Upsample(64)
