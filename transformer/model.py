49c2b6ee92460e324e54a8c72cb9a2db46909e0e
augustdemi <augustdemi@gmail.com>
2021-07-02 01:35:18 -0400

    runid8: max pool the logit from the enc_out

--- transformer/model.py
+++ transformer/model.py
@@ -53,10 +53,11 @@ class EncoderX(nn.Module):
 
 
     def forward(self, src, src_mask):
-        logit_token = Variable(torch.FloatTensor(np.random.rand(src.shape[0], 1, self.d_model))).to(src.device)
-        src_emb = torch.cat((logit_token, self.embed_fn(src)), dim=1)
+        # logit_token = Variable(torch.FloatTensor(np.random.rand(src.shape[0], 1, self.d_model))).to(src.device)
+        # src_emb = torch.cat((logit_token, self.embed_fn(src)), dim=1)
+        src_emb = self.embed_fn(src)
         enc_out = self.encoder(src_emb, src_mask) # bs, 1+8, 512
-        logit = self.fc(enc_out[:,0])
+        logit = self.fc(enc_out).max(1)[0] # pooling the latent dist logit throughout the "time step" dim
 
         return enc_out[:,1:], logit
 
@@ -84,10 +85,11 @@ class EncoderY(nn.Module):
 
 
     def forward(self, src_trg, src_trg_mask):
-        logit_token = Variable(torch.FloatTensor(np.random.rand(src_trg.shape[0], 1, self.d_model))).to(src_trg.device)
-        src_trg_emb = torch.cat((logit_token, self.embed_fn(src_trg)), dim=1)
+        # logit_token = Variable(torch.FloatTensor(np.random.rand(src_trg.shape[0], 1, self.d_model))).to(src_trg.device)
+        # src_trg_emb = torch.cat((logit_token, self.embed_fn(src_trg)), dim=1)
+        src_trg_emb = self.embed_fn(src_trg)
         enc_out = self.encoder(src_trg_emb, src_trg_mask) # bs, 1+8, 512
-        logit = self.fc(enc_out[:,0])
+        logit = self.fc(enc_out).max(1)[0] # pooling the latent dist logit throughout the "time step" dim
 
         return enc_out[:,1:], logit
 
@@ -118,24 +120,8 @@ class DecoderY(nn.Module):
                 nn.init.xavier_uniform_(p)
 
 
-    def forward(self, enc_out, latents, trg, src_mask, trg_mask, seq_start_end):
-        bs = enc_out.shape[0]
-        max_n_agents = (seq_start_end[:,1] - seq_start_end[:,0]).max()
-        src_mask = torch.zeros((bs, 8*max_n_agents))
-        enc_out_neighbors = torch.zeros((bs, 8*max_n_agents, self.d_model))
-
-        i = 0
-        for seq in seq_start_end:
-                # aa_enc_out.append(enc_out[s[0]:s[1]].reshape(-1, self.d_model).unsqueeze(0).repeat((s[1]-s[0]), 1, 1))
-                curr_seq_all_agents = enc_out[seq[0]:seq[1]].reshape(-1, self.d_model)
-                for a in range(seq[1]-seq[0]):
-                    enc_out_neighbors[i, :curr_seq_all_agents.shape[0]] = curr_seq_all_agents
-                    src_mask[i, :curr_seq_all_agents.shape[0]] = 0.5
-                    src_mask[i, 8*a : 8*(a+1)] = 1
-                    i+=1
-
-        src_mask = src_mask.unsqueeze(1).repeat((1,trg.shape[1],1))
-        dec_out =  self.decoder(self.trg_embed(trg), enc_out_neighbors.to(enc_out.device), latents.unsqueeze(1), src_mask.to(enc_out.device), trg_mask) # bs, 12, 512
+    def forward(self, enc_out, latents, trg, src_mask, trg_mask):
+        dec_out =  self.decoder(self.trg_embed(trg), enc_out, latents.unsqueeze(1), src_mask, trg_mask) # bs, 12, 512
         stats = self.fc(dec_out) # bs, 12, out*2
         mu = stats[:,:,:self.dec_out_size]
         logvar = stats[:,:,self.dec_out_size:]
