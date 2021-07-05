56d72dab5c7e3208c28fc0abe67b78346c7b0546
augustdemi <augustdemi@gmail.com>
2021-07-05 08:29:44 -0400

    dist_weight flag for score 추가

--- transformer/functional.py
+++ transformer/functional.py
@@ -26,14 +26,14 @@ def subsequent_mask(size):
     return torch.from_numpy(mask) == 0
 
 
-def attention(query, key, value, mask=None, dropout=None):
+def attention(query, key, value, mask=None, dropout=None, dist_weight=False):
     """
     Compute 'Scaled Dot Product Attention'
     """
     d_k = query.size(-1)
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k) # bs, # head, # query, # key = 70,8, 12, 7 ==> neighbors 여러개(k)의 src를 동시에 주면 70, 8,12,7k의 shape 될것. mask는 k=1에 대해서는 mask=True, k>1인 이웃이면 mask=False이되, value를 0.5로 설정
     if mask is not None: # mask = # 70, 1, 1, 7 for encoder, [70, 1, 12, 12] for decoder
-        if 0.5 in mask:
+        if dist_weight:
             scores = scores * mask.repeat(1, scores.shape[1], 1,1)
         scores = scores.masked_fill_(mask == 0, value=-1e9) # mask 1인 src의 mask에서는 모두 False로 전달될것. 따라서 score가 그대로 유지됨.  mask=True가 있는 trg mask에서는 mask==0이 False를 밷어서 score가 그대로. mask=False인 값에서는 score가 매우 적어질것.
     p_attn = softmax(scores, dim=-1)
--- transformer/multihead_attention.py
+++ transformer/multihead_attention.py
@@ -7,7 +7,7 @@ from .functional import clones, attention
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, h, d_model, dropout=0.1):
+    def __init__(self, h, d_model, dropout=0.1, dist_weight=False):
         """
         Take in model size and number of heads.
         """
@@ -19,6 +19,7 @@ class MultiHeadAttention(nn.Module):
         self.linears = clones(nn.Linear(d_model, d_model), 4)
         self.attn = None
         self.dropout = nn.Dropout(p=dropout)
+        self.dist_weight = dist_weight
 
     def forward(self, query, key, value, mask=None):
         """
@@ -32,7 +33,7 @@ class MultiHeadAttention(nn.Module):
         query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for l, x in
                              zip(self.linears, (query, key, value))]
         # 2) Apply attention on all the projected vectors in batch.
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout) #(bs, 8, # query, 64), (bs, 8, # query, # key)
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout, dist_weight = self.dist_weight) #(bs, 8, # query, 64), (bs, 8, # query, # key)
         # 3) "Concat" using a view and apply a final linear.
         x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k) #(bs, #query, 512)
         return self.linears[-1](x)
