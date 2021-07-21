ce96ba78b73518ae5f40b713ec727a23755fdd82
augustdemi <augustdemi@gmail.com>
2021-07-21 09:23:15 -0400

    200: with map

--- model.py
+++ model.py
@@ -63,6 +63,45 @@ def load_map_encoder(device, map_feat_dim):
     return map_encoder
 
 
+def crop(map, target_pos, inv_h_t, context_size=198):
+    # context_size=32
+    expanded_obs_img = np.full((map.shape[0] + context_size, map.shape[1] + context_size), False, dtype=np.float32)
+    expanded_obs_img[context_size//2:-context_size//2, context_size//2:-context_size//2] = map.astype(np.float32) # 99~-99
+
+
+    target_pixel = np.matmul(np.concatenate([target_pos.detach().cpu().numpy(), np.ones((len(target_pos), 1))], axis=1), inv_h_t)
+    target_pixel /= np.expand_dims(target_pixel[:, 2], 1)
+    target_pixel = target_pixel[:,:2]
+
+    # plt.imshow(map)
+    # for i in range(len(target_pixel)):
+    #     plt.scatter(target_pixel[i][0], target_pixel[i][1], c='r', s=1)
+    # plt.show()
+
+    img_pts = context_size//2 + np.round(target_pixel).astype(int)
+
+    # if (img_pts[i][0] < context_size // 2)
+
+    nearby_area = context_size//2
+    cropped_img = []
+    for i in range(target_pos.shape[0]):
+        im = expanded_obs_img[img_pts[i, 1] - nearby_area: img_pts[i, 1] + nearby_area,
+        img_pts[i, 0] - nearby_area: img_pts[i, 0] + nearby_area]
+        if np.prod(im.shape) != context_size**2:
+            im = np.ones((context_size, context_size))
+        cropped_img.append(transforms.Compose([
+            # transforms.Resize(32),
+            transforms.ToTensor()
+        ])(Image.fromarray(im)))
+    cropped_img = torch.stack(cropped_img, axis=0)
+
+    cropped_img[:,: , nearby_area, nearby_area] = 0
+
+    # plt.imshow(cropped_img[0,0])
+    # plt.show()
+
+    return cropped_img
+
 ###############################################################################
 # -----------------------------------------------------------------------
 
@@ -82,7 +121,7 @@ class Encoder(nn.Module):
         n_state=6
 
         self.rnn_encoder = nn.LSTM(
-            input_size=n_state, hidden_size=enc_h_dim
+            input_size=n_state + map_feat_dim, hidden_size=enc_h_dim
         )
 
         input_dim = enc_h_dim
@@ -94,7 +133,7 @@ class Encoder(nn.Module):
             dropout=dropout_mlp
         )
         self.fc2 = nn.Linear(mlp_dim, zS_dim)
-        # self.map_encoder = load_map_encoder(device, map_feat_dim)
+        self.map_encoder = load_map_encoder(device, map_feat_dim)
 
 
     def forward(self, obs_traj_st, seq_start_end, map, obs_vel, train=False):
@@ -104,8 +143,11 @@ class Encoder(nn.Module):
         Output:
         - final_h: Tensor of shape (self.num_layers, batch, self.h_dim)
         """
+        map_feat = self.map_encoder(obs_vel.reshape(-1, 2), map.reshape(-1, map.shape[2], map.shape[3], map.shape[4]), train=False)
+        map_feat = map_feat.reshape((8, -1, map_feat.shape[-1]))
 
-        _, (final_encoder_h, _) = self.rnn_encoder(obs_traj_st) # [8, 656, 16], 두개의 [1, 656, 32]
+        rnn_input = torch.cat((obs_traj_st, map_feat), dim=-1)
+        _, (final_encoder_h, _) = self.rnn_encoder(rnn_input) # [8, 656, 16], 두개의 [1, 656, 32]
 
         final_encoder_h = F.dropout(final_encoder_h,
                             p=self.dropout_rnn,
@@ -139,7 +181,7 @@ class EncoderY(nn.Module):
         self.dropout_rnn=dropout_rnn
 
         self.rnn_encoder = nn.LSTM(
-            input_size=n_pred_state, hidden_size=enc_h_dim, num_layers=1, bidirectional=True
+            input_size=n_pred_state + map_feat_dim, hidden_size=enc_h_dim, num_layers=1, bidirectional=True
         )
 
         input_dim = enc_h_dim*4 + mlp_dim
@@ -155,6 +197,7 @@ class EncoderY(nn.Module):
 
         self.initial_h_model = nn.Linear(n_state, enc_h_dim)
         self.initial_c_model = nn.Linear(n_state, enc_h_dim)
+        self.map_encoder = load_map_encoder(device, map_feat_dim)
 
 
     def forward(self, last_obs_traj_st, fut_vel_st, seq_start_end, obs_enc_feat, map, fut_vel, train=False):
@@ -173,7 +216,11 @@ class EncoderY(nn.Module):
         initial_c = torch.stack([initial_c, torch.zeros_like(initial_c, device=self.device)], dim=0)
         state_tuple=(initial_h, initial_c)
 
-        _, state = self.rnn_encoder(fut_vel_st, state_tuple)
+        map_feat = self.map_encoder(fut_vel.reshape(-1, 2), map.reshape(-1, map.shape[2], map.shape[3], map.shape[4]), train=False)
+        map_feat = map_feat.reshape((12, -1, map_feat.shape[-1]))
+
+        rnn_input = torch.cat((fut_vel_st, map_feat), dim=-1)
+        _, state = self.rnn_encoder(rnn_input, state_tuple)
 
         state = torch.cat(state, dim=0).permute(1, 0, 2)  # 2,81,32두개 -> 4, 81,32 -> 81,4,32
         state_size = state.size()
@@ -219,7 +266,7 @@ class Decoder(nn.Module):
         self.to_vel = nn.Linear(n_state, n_pred_state)
 
         self.rnn_decoder = nn.GRUCell(
-            input_size=mlp_dim + z_dim + n_pred_state, hidden_size=dec_h_dim
+            input_size=mlp_dim + z_dim + n_pred_state + d_map_feat, hidden_size=dec_h_dim
         )
 
         # self.mlp = make_mlp(
@@ -231,6 +278,7 @@ class Decoder(nn.Module):
 
         self.fc_mu = nn.Linear(dec_h_dim, n_pred_state)
         self.fc_std = nn.Linear(dec_h_dim, n_pred_state)
+        self.map_encoder = load_map_encoder(device, map_feat_dim)
 
 
     def forward(self, last_obs_traj_st, enc_h_feat, z, last_obs_and_fut_map, fut_traj=None, map_info=None):
@@ -252,11 +300,16 @@ class Decoder(nn.Module):
         a = self.to_vel(last_obs_traj_st)
         # a = self.to_vel(torch.cat((last_obs_traj_st, map[0]), dim=-1)) # map[0] = last observed map
 
+        if fut_traj is None:
+            seq_start_end, map_path, inv_h_t, integrate_fn = map_info[0], map_info[1], map_info[2], map_info[3]
+
         mus = []
         stds = []
+        map = last_obs_and_fut_map[0]
         for i in range(self.seq_len):
+            map_feat = self.map_encoder(a, map, train=False)
 
-            decoder_h= self.rnn_decoder(torch.cat([zx, a], dim=1), decoder_h) #493, 128
+            decoder_h= self.rnn_decoder(torch.cat([zx, a, map_feat], dim=1), decoder_h) #493, 128
             mu= self.fc_mu(decoder_h)
             logVar = self.fc_std(decoder_h)
             std = torch.sqrt(torch.exp(logVar))
@@ -265,9 +318,19 @@ class Decoder(nn.Module):
 
             if fut_traj is not None:
                 a = fut_traj[i,:,2:4]
+                map = last_obs_and_fut_map[i+1]
             else:
                 a = Normal(mu, std).rsample()
-
+                ####
+                pred_fut_traj = integrate_fn(a.unsqueeze(0)).squeeze(0)
+                map = []
+                for j, (s, e) in enumerate(seq_start_end):
+                    seq_map = imageio.imread(map_path[j])  # seq = 한 씬에서 모든 neighbors니까. 같은 데이터셋.
+                    seq_cropped_map = crop(seq_map, pred_fut_traj[s:e], inv_h_t[j],
+                                           context_size=self.map_size)  # (e-s), 1, 64, 64
+                    map.append(seq_cropped_map)
+                map = torch.cat(map).to(self.device)
+                ####
 
         mus = torch.stack(mus, dim=0)
         stds = torch.stack(stds, dim=0)
