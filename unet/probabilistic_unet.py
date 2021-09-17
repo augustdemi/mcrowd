4727c60cdae0a8a2eb6e74ab33bbee314600b708
augustdemi <augustdemi@gmail.com>
2021-09-17 07:31:27 -0400

    runid14: bottleneck, less training data, two AxisAlignGaussian, "clamp"

--- unet/probabilistic_unet.py
+++ unet/probabilistic_unet.py
@@ -22,6 +22,9 @@ class Encoder(nn.Module):
         self.input_channels = input_channels
         self.num_filters = num_filters
 
+        if posterior:
+            # To accomodate for the mask that is concatenated at the channel axis, we increase the input_channels.
+            self.input_channels += num_classes
 
         layers = []
         for i in range(len(self.num_filters)):
@@ -70,70 +73,12 @@ class AxisAlignedConvGaussian(nn.Module):
             self.name = 'Prior'
         self.encoder = Encoder(self.input_channels, self.num_filters, self.no_convs_per_block, num_classes=num_classes,
                                posterior=self.posterior)
-        self.conv_layer = nn.Conv2d(num_filters[-1] * 2, 2 * self.latent_dim, (1, 1), stride=1)
-
-
-        nn.init.kaiming_normal_(self.conv_layer.weight, mode='fan_in', nonlinearity='relu')
-        nn.init.normal_(self.conv_layer.bias)
-
-    def forward(self, input, hx):
-
-        encoding = self.encoder(input)  # [4, 64, 10, 10]
-        # self.show_enc = encoding
-        encoding=torch.cat([hx, encoding], dim=1)
-
-        # We only want the mean of the resulting hxw image
-        encoding = torch.mean(encoding, dim=2, keepdim=True)
-        encoding = torch.mean(encoding, dim=3, keepdim=True)
-
-        # Convert encoding to 2 x latent dim and split up for mu and log_sigma
-        mu_log_sigma = self.conv_layer(encoding)
-
-        # We squeeze the second dimension twice, since otherwise it won't work when batch size is equal to 1
-        mu_log_sigma = torch.squeeze(mu_log_sigma, dim=2)
-        mu_log_sigma = torch.squeeze(mu_log_sigma, dim=2)
-
-        mu = mu_log_sigma[:, :self.latent_dim]
-        log_var = mu_log_sigma[:, self.latent_dim:]
-
-        # This is a multivariate normal with diagonal covariance matrix sigma
-        # https://github.com/pytorch/pytorch/pull/11178
-        dist = Independent(Normal(loc=mu, scale=torch.sqrt(torch.exp(log_var))), 1)
-        return dist
-
-
-
-
-class AxisAlignedConvGaussian_prior(nn.Module):
-    """
-    A convolutional net that parametrizes a Gaussian distribution with axis aligned covariance matrix.
-    """
-
-    def __init__(self, input_channels, num_filters, no_convs_per_block, latent_dim, num_classes=2, posterior=False):
-        super(AxisAlignedConvGaussian_prior, self).__init__()
-        self.input_channels = input_channels
-        self.channel_axis = 1
-        self.num_filters = num_filters
-        self.no_convs_per_block = no_convs_per_block
-        self.latent_dim = latent_dim
-        self.posterior = posterior
-        if self.posterior:
-            self.name = 'Posterior'
-        else:
-            self.name = 'Prior'
-
-
-
-        self.encoder = nn.Sequential(
-            nn.Conv2d(num_filters[-1], num_filters[-1], (1, 1), stride=1),
-            nn.ReLU(inplace=True),
-            nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
-            nn.Conv2d(num_filters[-1], num_filters[-1], (1, 1), stride=1),
-            nn.ReLU(inplace=True),
-        )
         self.conv_layer = nn.Conv2d(num_filters[-1], 2 * self.latent_dim, (1, 1), stride=1)
-
-
+        self.show_img = 0
+        self.show_seg = 0
+        self.show_concat = 0
+        self.show_enc = 0
+        self.sum_input = 0
 
         nn.init.kaiming_normal_(self.conv_layer.weight, mode='fan_in', nonlinearity='relu')
         nn.init.normal_(self.conv_layer.bias)
@@ -142,9 +87,13 @@ class AxisAlignedConvGaussian_prior(nn.Module):
 
         # If segmentation is not none, concatenate the mask to the channel axis of the input
         if segm is not None:
+            self.show_img = input
+            self.show_seg = segm
             input = torch.cat((input, segm), dim=1)
+            self.show_concat = input
+            self.sum_input = torch.sum(input)
 
-        encoding = self.encoder(input)  # [4, 64, 5, 5]
+        encoding = self.encoder(input)  # [4, 128, 10, 10]
         # self.show_enc = encoding
 
         # We only want the mean of the resulting hxw image
@@ -165,7 +114,6 @@ class AxisAlignedConvGaussian_prior(nn.Module):
         # https://github.com/pytorch/pytorch/pull/11178
         dist = Independent(Normal(loc=mu, scale=torch.sqrt(torch.exp(log_var))), 1)
         return dist
-
 
 
 class Fcomb(nn.Module):
@@ -264,7 +212,7 @@ class ProbabilisticUnet(nn.Module):
 
         self.unet = Unet(self.input_channels, self.num_classes, self.num_filters, apply_last_layer=False,
                          padding=True).to(device)
-        self.prior = AxisAlignedConvGaussian_prior(self.input_channels, self.num_filters, self.no_convs_per_block,
+        self.prior = AxisAlignedConvGaussian(self.input_channels, self.num_filters, self.no_convs_per_block,
                                              self.latent_dim).to(device)
         self.posterior = AxisAlignedConvGaussian(self.input_channels, self.num_filters, self.no_convs_per_block,
                                                  self.latent_dim, num_classes=num_classes, posterior=True).to(device)
@@ -282,8 +230,8 @@ class ProbabilisticUnet(nn.Module):
 
         # latent dist
         if training:
-            self.posterior_latent_space = self.posterior.forward(patch, self.unet_enc_feat)
-        self.prior_latent_space = self.prior.forward(self.unet_enc_feat)
+            self.posterior_latent_space = self.posterior.forward(patch, segm)
+        self.prior_latent_space = self.prior.forward(patch)
 
         if training:
             z = self.posterior_latent_space.rsample()
