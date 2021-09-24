98e62ac700a693dbbbd7aafef7287907ed50b8df
augustdemi <augustdemi@gmail.com>
2021-09-24 10:15:14 -0400

    path finding / eth분류

--- data/loader.py
+++ data/loader.py
@@ -1,20 +1,35 @@
 from torch.utils.data import DataLoader
 
 from .trajectories import TrajectoryDataset, seq_collate
+from .eth_trajectories import TrajectoryDataset as eth_Traj
+from .eth_trajectories import seq_collate as eth_seq_collate
 
 def data_loader(args, path, shuffle=True):
-    dset = TrajectoryDataset(
-        path,
-        obs_len=args.obs_len,
-        pred_len=args.pred_len,
-        skip=args.skip,
-        delim=args.delim,
-        device=args.device)
+
+    if 'Trajectories' in path:
+        dset = TrajectoryDataset(
+            path,
+            obs_len=args.obs_len,
+            pred_len=args.pred_len,
+            skip=args.skip,
+            delim=args.delim,
+            device=args.device)
+        seq_col = seq_collate
+    else:
+        dset = eth_Traj(
+            path,
+            obs_len=args.obs_len,
+            pred_len=args.pred_len,
+            skip=args.skip,
+            delim=args.delim,
+            device=args.device)
+        seq_col = eth_seq_collate
+
 
     loader = DataLoader(
         dset,
         batch_size=args.batch_size,
         shuffle=shuffle,
         num_workers=args.loader_num_workers,
-        collate_fn=seq_collate)
+        collate_fn=seq_col)
     return dset, loader
