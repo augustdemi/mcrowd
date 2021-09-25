5e65c34e110ee78826d2e15212270da95671c11d
augustdemi <augustdemi@gmail.com>
2021-09-24 23:06:16 -0400

    lgcvae2.runid2: lg AE of pathfind에서 로드하도록 - bug fixed

--- eth/solver.py
+++ eth/solver.py
@@ -167,7 +167,7 @@ class Solver(object):
             # # input = env + 8 past / output = env + lg
 
             if args.load_e > 0:
-                lg_cvae_path = 'lgcvae_enc_block_1_fcomb_block_2_wD_20_lr_0.001_a_0.25_r_2.0_run_24_lines'
+                lg_cvae_path = 'lgcvae_enc_block_1_fcomb_block_2_wD_20_lr_0.001_a_0.25_r_2.0_run_24'
                 lg_cvae_path = os.path.join('../ckpts', lg_cvae_path, 'iter_3400_lg_cvae.pt')
 
                 if self.device == 'cuda':
