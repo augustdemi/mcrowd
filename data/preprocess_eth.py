07ceaf5ab08c5c02ae6a86a71fabd9cd8586057a
augustdemi <augustdemi@gmail.com>
2021-09-26 09:14:30 -0400

    continue bug

--- data/preprocess_eth.py
+++ data/preprocess_eth.py
@@ -85,8 +85,8 @@ class TrajectoryDataset(Dataset):
             # if 'zara' in path or 'eth' in path or 'hotel' in path:
             # if 'zara' in path or 'hotel' in path or '003' in path:
             #     continue
-            if 'students003' in path:
-                continue
+            # if 'students003' in path:
+            #     continue
             if 'zara01' in path.split(deli)[-1]:
                 map_file_name = 'zara01'
             elif 'zara02' in path.split(deli)[-1]:
