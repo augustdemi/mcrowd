4ad454a02a9b0541ab28f037000cebd03f9f0001
augustdemi <augustdemi@gmail.com>
2021-09-26 20:38:50 -0400

    lg AE 8 check feat수정

--- solver.py
+++ solver.py
@@ -459,6 +459,12 @@ class Solver(object):
                  obs_frames, pred_frames, map_path, inv_h_t,
                  local_map, local_ic, local_homo, local_map_size) = batch
 
+
+                batch = data_loader.dataset.__getitem__(40)
+                (obs_traj, fut_traj,
+                 obs_frames, pred_frames, map_path, inv_h_t,
+                 local_map, local_ic, local_homo, local_map_size) = batch
+
                 obs_heat_map, lg_heat_map = self.make_heatmap(local_ic, local_map, local_map_size)
 
                 self.lg_cvae.forward(obs_heat_map, None, training=False)
@@ -489,6 +495,7 @@ class Solver(object):
                     ax.axis('off')
 
                 ###################################################
+                # -------- long term goal --------
                 # -------- long term goal --------
                 self.lg_cvae.forward(obs_heat_map, None, training=False)
 
@@ -521,10 +528,12 @@ class Solver(object):
                 heat_map_traj = np.zeros((local_map_size[i], local_map_size[i]))
                 for t in [0, 1, 2, 3, 4, 5, 6, 7, 11, 15, 19]:
                     heat_map_traj[local_ic[i, t, 0], local_ic[i, t, 1]] = 20
+                heat_map_traj = ndimage.filters.gaussian_filter(heat_map_traj, sigma=2)
                 heat_map_traj = resize(heat_map_traj, (160, 160))
-                heat_map_traj /= heat_map_traj.sum()
                 # as Y-net used variance 4 for the GT heatmap representation.
                 heat_map_traj = ndimage.filters.gaussian_filter(heat_map_traj, sigma=2)
+                heat_map_traj /= heat_map_traj.max()
+                # plt.imshow(heat_map_traj)
 
 
                 fig = plt.figure(figsize=(15, 10))
@@ -535,7 +544,7 @@ class Solver(object):
                     if k < 5:
                         a = mm[k][i, 0].detach().cpu().numpy().copy()
                         # ax.imshow(np.stack([env * (1 - heat_map_traj), env * (1 - a * 5), env], axis=2))
-                        ax.imshow(np.stack([heat_map_traj, env * (1 - a * 5), env], axis=2))
+                        ax.imshow(np.stack([env * (1 - heat_map_traj * 3), env * (1 - a * 20), env], axis=2))
                     else:
                         ax.imshow(mm[k % 5][i, 0])
 
@@ -544,10 +553,12 @@ class Solver(object):
                     ax.set_title('prior' + str(k % 5 + 6))
                     if k < 5:
                         a = mmm[k][i, 0].detach().cpu().numpy().copy()
-                        ax.imshow(np.stack([env * (1 - heat_map_traj), env * (1 - a * 5), env], axis=2))
+                        ax.imshow(np.stack([env * (1 - heat_map_traj * 3), env * (1 - a * 20), env], axis=2))
                         # ax.imshow(np.stack([1-env, 1-heat_map_traj, 1 - mmm[k][i, 0] / (0.1*mmm[k][i, 0].max())],axis=2))
                     else:
                         ax.imshow(mmm[k % 5][i, 0])
+
+
 
                 # ======================== min /max of LG ===============================
 
@@ -1298,4 +1309,6 @@ class Solver(object):
             self.lg_cvae = torch.load(lg_cvae_path)
 
         else:
+            lg_cvae_path = 'lgcvae.' + self.dataset_name + '_enc_block_1_fcomb_block_2_wD_20_lr_0.001_a_0.25_r_2.0_skip_0_run_8'
+            lg_cvae_path = os.path.join('ckpts', lg_cvae_path, 'iter_5400_lg_cvae.pt')
             self.lg_cvae = torch.load(lg_cvae_path, map_location='cpu')
