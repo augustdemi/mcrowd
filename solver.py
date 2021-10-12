0d605462351aff4e7948f93680bb5ebf9db365e7
augustdemi <augustdemi@gmail.com>
2021-10-12 18:38:05 -0400

    [SDD] sg 8 - bug fixed

--- solver.py
+++ solver.py
@@ -162,13 +162,9 @@ class Solver(object):
         self.num_layers = args.num_layers
         self.decoder_h_dim = args.decoder_h_dim
 
-        # lg_cvae_path = 'sdd.lgcvae_enc_block_1_fcomb_block_2_wD_20_lr_0.0001_lg_klw_1.0_a_0.25_r_2.0_fb_0.5_anneal_e_0_aug_1_run_181'
-        # lg_cvae_path = os.path.join('ckpts', lg_cvae_path, 'iter_43000_lg_cvae.pt')
 
-
-        lg_cvae_path = 'sdd.lgcvae_enc_block_1_fcomb_block_2_wD_20_lr_0.001_lg_klw_1.0_a_0.25_r_2.0_fb_0.5_anneal_e_20_aug_1_run_181'
-        lg_cvae_path = os.path.join('ckpts', lg_cvae_path, 'iter_25000_lg_cvae.pt')
-
+        lg_cvae_path = 'sdd.lgcvae_enc_block_1_fcomb_block_2_wD_20_lr_0.0001_lg_klw_1.0_a_0.25_r_2.0_fb_4.0_anneal_e_10_aug_1_run_23'
+        lg_cvae_path = os.path.join('ckpts', lg_cvae_path, 'iter_21500_lg_cvae.pt')
 
         if self.device == 'cuda':
             self.lg_cvae = torch.load(lg_cvae_path)
@@ -230,9 +226,9 @@ class Solver(object):
         for i in range(len(local_ic)):
             map_size = local_map[i][0].shape[0]
             if map_size < down_size:
-                env = np.full((down_size,down_size),1)
+                env = np.full((down_size,down_size),3)
                 env[half-map_size//2:half+map_size//2, half-map_size//2:half+map_size//2] = local_map[i][0]
-                ohm = [env]
+                ohm = [env/5]
                 heat_map_traj = np.zeros_like(local_map[i][0])
                 heat_map_traj[local_ic[i, :self.obs_len, 0], local_ic[i, :self.obs_len, 1]] = 1
                 heat_map_traj= ndimage.filters.gaussian_filter(heat_map_traj, sigma=2)
@@ -251,7 +247,7 @@ class Solver(object):
                 heat_maps.append(np.stack(ohm))
             else:
                 env = cv2.resize(local_map[i][0], dsize=(down_size, down_size))
-                ohm = [env]
+                ohm = [env/5]
                 heat_map_traj = np.zeros_like(local_map[i][0])
                 heat_map_traj[local_ic[i, :self.obs_len, 0], local_ic[i, :self.obs_len, 1]] = 100
 
