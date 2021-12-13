b223cd347b8204e662e811afcce7cd38eea0aae7
augustdemi <augustdemi@gmail.com>
2021-12-13 18:08:43 -0500

    [path] lgcvae ae 300 : with starting pos heatmap - bug

--- solver.py
+++ solver.py
@@ -272,7 +272,7 @@ class Solver(object):
             #          TRAIN THE VAE (ENC & DEC)
             # ============================================
 
-            (obs_traj, fut_traj, seq_start_end,
+            (obs_traj, fut_traj, _,_,seq_start_end,
              obs_frames, pred_frames, map_path, inv_h_t,
              local_map, local_ic, local_homo) = next(iterator)
             batch_size = obs_traj.size(1) #=sum(seq_start_end[:,1] - seq_start_end[:,0])
@@ -372,7 +372,7 @@ class Solver(object):
             b=0
             for batch in data_loader:
                 b+=1
-                (obs_traj, fut_traj, seq_start_end,
+                (obs_traj, fut_traj, _,_,seq_start_end,
                  obs_frames, pred_frames, map_path, inv_h_t,
                  local_map, local_ic, local_homo) = batch
                 batch_size = obs_traj.size(1)
