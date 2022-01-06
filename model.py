5b1632091884488ce7917fe016499ae8693f4e2e
augustdemi <augustdemi@gmail.com>
2022-01-06 01:50:39 -0500

    [nu] traj 9

--- model.py
+++ model.py
@@ -282,10 +282,7 @@ class Decoder(nn.Module):
 
             mus.append(mu)
             stds.append(std)
-            if fut_vel_st is not None:
-                pred_vel = fut_vel_st[i]
-            else:
-                pred_vel = Normal(mu, std).rsample()
+            pred_vel = Normal(mu, std).rsample()
 
             # create context for the next prediction
             curr_pos = pred_vel * self.scale * self.dt + last_pos
