23b31078b7bbf7972201c9e00c0eed5d1bb84c34
ml1323 <64507714+ml1323@users.noreply.github.com>
2022-03-29 04:44:38 +0900

    nu traj 64 - eval1: sg 간 거리체크, micro에선 dist에서 rsample대신 중간 pred그대로 가져오게, 단 sg에선 pred_vel가 tf값으로 주어짐.

--- model.py
+++ model.py
@@ -327,7 +327,7 @@ class Decoder(nn.Module):
         sg_h = torch.cat(sg_h, dim=0).permute(1, 0, 2)
         sg_h = F.dropout(sg_h,
                         p=self.dropout_rnn,
-                        training=train)  # [bs, max_time, enc_rnn_dim]
+                        training=False)  # [bs, max_time, enc_rnn_dim]
         sg_feat = self.sg_fc(sg_h.reshape(-1, 4 * self.enc_h_dim))
 
 
@@ -348,10 +348,8 @@ class Decoder(nn.Module):
             if (i == sg_update_idx[j]):
                 pred_vel = sg_state[j + 1, :, 2:4]
                 j += 1
+                all_pred.append(pred_vel)
             else:
-                pred_vel = Normal(mu, std).rsample()
-
-            if self.context_dim > 0:
                 pred_vel = Normal(mu, std).rsample()
                 # create context for the next prediction
                 curr_pos = pred_vel * self.scale * self.dt + last_pos
@@ -364,17 +362,7 @@ class Decoder(nn.Module):
                 logVar = torch.clamp(logVar, max=8e1)
                 std = torch.clamp(torch.sqrt(torch.exp(logVar)), min=1e-8)
 
-                if i in sg_update_idx:
-                    pred_vel_cand = Normal(mu, std).rsample((20,))
-                    refined_pred_vel = []
-                    for agent_idx in range(len(pred_vel)):
-                        cand_pos = pred_vel_cand[:, agent_idx] * self.scale * self.dt + last_pos[agent_idx]
-                        dist_diff = ((cand_pos - curr_pos[agent_idx]) ** 2).sum(1)
-                        pred_vel_idx = torch.argmin(dist_diff)
-                        refined_pred_vel.append(pred_vel_cand[pred_vel_idx, agent_idx])
-                    pred_vel = torch.stack(refined_pred_vel)
-                else:
-                    pred_vel = Normal(mu, std).rsample()
+                pred_vel = Normal(mu, std).rsample()
 
                 curr_pos = pred_vel * self.scale * self.dt + last_pos
                 last_pos = curr_pos
