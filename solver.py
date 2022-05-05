large lg ae 8 - kl 분산 bug

solver.py
@@ -290,7 +290,7 @@ class Solver(object):
                 recon_lg_heat ** self.gamma)).sum().div(batch_size)
 
 
-            lg_kl =  lg_kl.div(batch_size)
+            lg_kl =  sum(lg_kl).div(batch_size)
 
             lg_elbo = focal_loss
 
@@ -392,7 +392,7 @@ class Solver(object):
                     pred_lg_heat = F.normalize(pred_lg_heat.view(pred_lg_heat.shape[0], -1), p=1)
                     lg_heat_map = lg_heat_map.view(lg_heat_map.shape[0], -1)
 
-                    lg_kl += kl
+                    lg_kl += sum(kl).div(batch_size)
                     lg_recon += (self.alpha * lg_heat_map * torch.log(pred_lg_heat + self.eps) * ((1 - pred_lg_heat) ** self.gamma) \
                          + (1 - self.alpha) * (1 - lg_heat_map) * torch.log(1 - pred_lg_heat + self.eps) * (pred_lg_heat ** self.gamma)).sum().div(batch_size)
 